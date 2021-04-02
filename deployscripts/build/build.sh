9c446cb3143b06cff2fe4c4a6440f210ec3e0e65
tungvt22 <tungvt22@mail.viettel.com.vn>
2021-04-02 08:53:18 +0000

    fix: fix build scripts

--- deployscripts/build/build.sh
+++ deployscripts/build/build.sh
@@ -1,11 +1,12 @@
 rm -rf ~/build-temp
 mkdir -p ~/build-temp
 cp -a ./ ~/build-temp
-cp -a ./deployscripts/build/ ~/build-temp
+cp -a ./deployscripts/build/* ~/build-temp
 cd ~/build-temp
 git submodule update --init
-docker build \
---build-arg http_proxy=http://10.61.11.42:3128 \
---build-arg https_proxy=http://10.61.11.42:3128 \
--t 192.168.0.3:5000/vworkspace ~/build-temp
+ls -la
+# docker build \
+# --build-arg http_proxy=http://10.61.11.42:3128 \
+# --build-arg https_proxy=http://10.61.11.42:3128 \
+# -t 192.168.0.3:5000/vworkspace ~/build-temp
 rm -rf ~/build-temp
