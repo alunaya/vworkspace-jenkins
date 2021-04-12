d1e8ecdd43bef4f6c76eef1c78d7e8bb1f76c286
alunaya <tungvt22@viettel.com.vn>
2021-04-12 08:48:17 +0000

    fix: add push to registry step in build script

--- deployscripts/build/build.sh
+++ deployscripts/build/build.sh
@@ -6,9 +6,9 @@ docker build \
 --build-arg https_proxy=http://10.61.11.42:3128 \
 -t 192.168.0.3:5000/vworkspace .
 
+docker push 192.168.0.3:5000/vworkspace
 # Revert changes to modified files.
 git reset --hard
-
 # Remove all untracked files and directories.
 # '-f' is force, '-d' is remove directories.
 git clean -fd
