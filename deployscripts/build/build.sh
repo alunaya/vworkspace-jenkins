b63bb492b9affb49a3fb7a1cb2ac765c685720cd
alunaya <tungvt22@viettel.com.vn>
2021-04-12 08:01:56 +0000

    fix: remove copying project action from build script

--- deployscripts/build/build.sh
+++ deployscripts/build/build.sh
@@ -1,12 +1,14 @@
-rm -rf ~/build-temp
-mkdir -p ~/build-temp
-cp -a ./ ~/build-temp
-cp -a ./deployscripts/build/* ~/build-temp
-cd ~/build-temp
+cp -a ./deployscripts/build/* ./
 git submodule update --init
 ls -la
 docker build \
 --build-arg http_proxy=http://10.61.11.42:3128 \
 --build-arg https_proxy=http://10.61.11.42:3128 \
--t 192.168.0.3:5000/vworkspace ~/build-temp
-rm -rf ~/build-temp
+-t 192.168.0.3:5000/vworkspace .
+
+# Revert changes to modified files.
+git reset --hard
+
+# Remove all untracked files and directories.
+# '-f' is force, '-d' is remove directories.
+git clean -fd
